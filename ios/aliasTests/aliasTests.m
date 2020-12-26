04be29ec5078ea844073c42b7971bd1e4edfe381
Ospens <1ospens1@gmail.com>
2020-12-26 17:43:46 +0300

    Rename

--- ios/aliasTests/aliasTests.m
+++ ios/aliasTests/aliasTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface aliasTests : XCTestCase
+@interface AliasTests : XCTestCase
 
 @end
 
-@implementation aliasTests
+@implementation AliasTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
