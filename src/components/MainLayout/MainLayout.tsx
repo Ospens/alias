d612e3b89871513453f99caafe43b8c9c4a584dd
Ilya Yurkin <yurkin@innoscripta.com>
2022-04-07 12:18:59 +0300

    Added admob for ios

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,14 +1,21 @@
-import React, { FC, memo, useMemo } from "react";
+import React, { FC, memo, useMemo, useEffect } from "react";
 import { SafeAreaView, View } from "react-native";
+import { BannerAd, BannerAdSize, TestIds } from "@react-native-admob/admob";
+import { requestTrackingPermission } from "react-native-tracking-transparency";
 import styles from "./MainLayout.styles";
 import type { MainLayoutProps } from "./MainLayout.types";
 
 const MainLayout: FC<MainLayoutProps> = memo(({ children, style, bottomPanel }) => {
   const childrenStyle = useMemo(() => [styles.childrenWrapper, style], [style]);
 
+  useEffect(() => {
+    requestTrackingPermission();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={childrenStyle}>{children}</View>
+      <BannerAd size={BannerAdSize.ADAPTIVE_BANNER} unitId={TestIds.BANNER} />
       <View style={styles.bottomPanelWrapper}>{bottomPanel}</View>
     </SafeAreaView>
   );
